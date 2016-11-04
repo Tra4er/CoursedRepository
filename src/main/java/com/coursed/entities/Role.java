e7fd84f27557fc00b801c952693fa916452ea62f
Hexray <zikfrostfire@gmail.com>
2016-11-04 23:29:20 +0200

    Adding jpa info for Role

--- src/main/java/com/coursed/entities/Role.java
+++ src/main/java/com/coursed/entities/Role.java
@@ -1,15 +1,35 @@
 package com.coursed.entities;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 /**
  * Created by Hexray on 04.11.2016.
  */
+@Entity
 public class Role {
     @Id
     @GeneratedValue
     private Long id;
+    private String Name;
 
-    private String roleName;
+    public Role() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
 }
