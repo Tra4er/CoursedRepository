0c1274ddc9551b9419ece47fb6dec2be618067f5
Hexray <zikfrostfire@gmail.com>
2016-12-01 00:23:52 +0200

    Created possibility to add a new year through the jQuery and rest on back end

--- src/main/java/com/coursed/controller/rest/YearResource.java
+++ src/main/java/com/coursed/controller/rest/YearResource.java
@@ -2,32 +2,34 @@ package com.coursed.controller.rest;
 
 import com.coursed.model.Year;
 import com.coursed.service.YearService;
+import com.fasterxml.jackson.databind.util.JSONPObject;
+import org.apache.catalina.servlet4preview.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Collection;
+import java.util.concurrent.ExecutionException;
+import java.util.stream.Collectors;
 
 /**
  * Created by Hexray on 16.11.2016.
  */
-@RestController
-@RequestMapping("/api/years")
+@RestController("/api/years/")
 public class YearResource {
     @Autowired
     private YearService yearService;
 
-    @RequestMapping(value = "/getAll", method = RequestMethod.GET)
+    @RequestMapping(value = "getAll", method = RequestMethod.GET)
     private Collection<Year> getYears() {
         return yearService.findAll();
     }
 
-    @RequestMapping(value = "/create", method = RequestMethod.POST)
-    private void addYear(@RequestBody Year newYear) {
-        int a = 5;
-        yearService.create(newYear);
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    private void createYear(@RequestBody Year year) {
+        yearService.create(year);
     }
-
 }
--- src/main/resources/static/js/scriptsHex.js
+++ src/main/resources/static/js/scriptsHex.js
@@ -2,9 +2,15 @@
  * Created by Hexray on 27.11.2016.
  */
 const HOST = 'http://localhost:8080/';
-const API = HOST + "api";
+const API = "api";
+
+var token;
+var header;
 
 $(document).ready(function () {
+    token = $("meta[name='_csrf']").attr("content");
+    header = $("meta[name='_csrf_header']").attr("content");
+
     var titles = ['id', 'Рік початку', 'Рік завершення'];
     insertTable(titles, HOST + "html/frontLayout.html", "contentTable");
 
@@ -12,23 +18,25 @@ $(document).ready(function () {
     fillTableFrom("contentTable", API + "/years/getAll", entityParams);
 });
 
-$("#buttonYearPost").click(function () {
 
-
-    $.post(API + "/years/create",
-        {
-            yearBegin: $("#yearBegin").val(),
-            yearEnd: $("#yearEnd").val()
-        });
+$(document).ajaxSend(function(e, xhr, options) {
+    xhr.setRequestHeader(header, token);
 });
 
-//Loads '#tableId' structure from 'address', puts it into the 'mainContainer' and fills its <thead> with 'titleArray'
+$('#buttonYearPost').click(function(){
+    var form = $('#modal-body');
+
+    sendAjaxPost(form);
+});
+
+//It loads '#tableId' structure from 'address', puts it into the 'mainContainer' and fills its <thead> with 'titleArray'
 function insertTable(titleArray, address, tableId) {
     $.each(titleArray, function (i, title) {
         $("#" + tableId + "> thead > tr").append('<th>'+ title +'</th>');
     });
 }
 
+//It fills table's '#tableId' rows via the getting JSON from 'requestAddress' which contains selected keys in params
 function fillTableFrom(tableId, requestAddress, params) {
     $.getJSON(requestAddress, function(response){
         //Go through the each entity in the response
@@ -48,3 +56,37 @@ function fillTableFrom(tableId, requestAddress, params) {
     });
 }
 
+//Sends JSON which was extracted and generated from 'elem'
+function sendAjaxPost(element) {
+    $.ajax({
+        type: 'POST',
+        url: 'api/years/create',
+        contentType: "application/json",
+        data: JSON.stringify(element.serializeObject()),
+        success: function () {
+            alert("Успішно");
+        },
+        error: function (data) {
+            alert("Помилка!");
+            console.log(data)
+        }
+    });
+}
+
+//It extracts fields from the element to prepare them to be converted to the JSON
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
