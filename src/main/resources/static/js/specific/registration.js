15baee4b86412ca4180ae17a1c86aa30485e2014
Olena <alenagiv@gmail.com>
2016-12-01 17:16:42 +0200

    speciality fills in register form

--- src/main/resources/static/js/specific/registration.js
+++ src/main/resources/static/js/specific/registration.js
@@ -3,48 +3,30 @@
  */
 $(function () {
     fillSelectYear("YearId", API + "/years/getAll");
-
     fillSelectFrom("SpecialityId", API + "/specialities/getAll", "fullName");
 });
 
+// Заполнить выпадающи список годов
 function fillSelectYear(selectId, requestAddress) {
-    $.getJSON(requestAddress, function(response){
+    $.getJSON(requestAddress, function (response) {
         var htmlElements = "<option value='0'>Обрати рік</option>";
         //Go through the each entity in the response
         $.each(response, function (i, year) {
             htmlElements += "<option value='" + year.id + "'>" + year.beginYear + "-" + year.endYear + "</option>";
-            });
-            $("#" + selectId).html(htmlElements);
-        });
-    };
-
-function fillSelectFrom(selectId, requestAddress, param) {
-    $.getJSON(requestAddress, function(response){
-        var firstString = $("#" + selectId + ">option:first).text");
-        var htmlElements = "<option value='0'>firstString</option>";
-        //Go through the each entity in the response
-        $.each(response, function (i, entity) {
-            htmlElements += "<option value='" + entity.id + "'>" + entity.param +"</option>";
         });
         $("#" + selectId).html(htmlElements);
     });
-};
+}
 
-function fillSelectFrom2(selectId, requestAddress, params) {
-    $.getJSON(requestAddress, function(response){
+// заполнить выпадающий список на основе полученных данных
+function fillSelectFrom(selectId, requestAddress, param) {
+    $.getJSON(requestAddress, function (response) {
+        var firstString = $("#" + selectId + " > option:first").text();
+        var htmlElements = "<option value='0'> " + firstString + "</option>";
         //Go through the each entity in the response
         $.each(response, function (i, entity) {
-            var htmlRow = "<tr>";
-            //Go through the each parameter in the entity
-            $.each(entity, function (paramName, paramValue) {
-                //If a parameter belongs to params array(argument of function)
-                if($.inArray(paramName, params) !== -1) {
-                    //Then we add the value of this parameter to the row
-                    htmlRow += ("<td>" + paramValue + "</td>");
-                }
-            });
-            htmlRow += "</tr>";
-            $("#" + tableId + "> tbody").append(htmlRow);
+            htmlElements += "<option value='" + entity.id + "'>" + entity[param] + "</option>";
         });
+        $("#" + selectId).html(htmlElements);
     });
 }
