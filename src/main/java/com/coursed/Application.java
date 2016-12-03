4d87a41bb4b5d29e67a0fce9f733e44a8591a748
Tra4er <brysonua@gmail.com>
2016-12-03 17:38:54 +0200

    Added fields check on submit click.

--- src/main/java/com/coursed/Application.java
+++ src/main/java/com/coursed/Application.java
@@ -3,11 +3,21 @@ package com.coursed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
+
+	private static Class<Application> applicationClass = Application.class;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(applicationClass);
+	}
+
 }
