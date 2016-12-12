378f0f79f266c19e11f8f12762afb1983a0c73e4
Tra4er <brysonua@gmail.com>
2016-12-12 03:25:32 +0200

    Simple events out is done.

--- src/main/resources/static/js/specific/events.js
+++ src/main/resources/static/js/specific/events.js
@@ -11,27 +11,28 @@ function init(){
 }
 function fillTable(){
     var entityParams = ['id', 'beginDate', 'expirationDate', 'eventType'];
-    fillTableFrom("content-table", API + "/years/getAll", entityParams);
-    // var requestAddress = API + "/events/getAll";
-    // $.getJSON(requestAddress, function(response){
-    //     //Go through the each entity in the response
-    //     $.each(response, function (i, entity) {
-    //         var htmlRow = "<tr>";
-    //         //Go through the each parameter in the entity
-    //         $.each(entity, function (paramName, paramValue) {
-    //             //If a parameter belongs to params array(argument of function)
-    //             if($.inArray(paramName, entityParams) !== -1) {
-    //                 console.log("paramName: " + paramName);
-    //                 console.log("paramValue: " + paramValue);
-    //                 if(paramName.indexOf("Date") !== 1) {
-    //                     paramValue = new Date(paramValue);
-    //                 }
-    //                 //Then we add the value of this parameter to the row
-    //                 htmlRow += ("<td>" + paramValue + "</td>");
-    //             }
-    //         });
-    //         htmlRow += "</tr>";
-    //         $("#content-table> tbody").append(htmlRow);
-    //     });
-    // });
+    // fillTableFrom("content-table", API + "/events/getAll", entityParams);
+    var requestAddress = API + "/events/getAll";
+    $.getJSON(requestAddress, function(response){
+        //Go through the each entity in the response
+        $.each(response, function (i, entity) {
+            var htmlRow = "<tr>";
+            //Go through the each parameter in the entity
+            $.each(entity, function (paramName, paramValue) {
+                //If a parameter belongs to params array(argument of function)
+                if($.inArray(paramName, entityParams) !== -1) {
+                    if(paramName.includes("Date")) {
+                        // var date = new Date();
+                        // date.setTime(Date.parse(paramValue));
+                        paramValue = paramValue + "";
+                        paramValue = paramValue.replace("T", " Час: ");
+                    }
+                    //Then we add the value of this parameter to the row
+                    htmlRow += ("<td>" + paramValue + "</td>");
+                }
+            });
+            htmlRow += "</tr>";
+            $("#content-table> tbody").append(htmlRow);
+        });
+    });
 }
