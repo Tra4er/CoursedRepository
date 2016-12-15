22ab1b29ffdb9e7edfc046fc3c660ac05354affa
Hexray <zikfrostfire@gmail.com>
2016-12-16 00:23:28 +0200

    Localization for disciplines. Added missed enum value for DisciplineType

---
+++ src/main/resources/static/js/local/disciplines-local-ukr.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Hexray on 16.12.2016.
+ */
+var localDisciplineUkr = {};
+//EngToUkr
+localDisciplineUkr["EXAM"] = "Екзамен";
+localDisciplineUkr["CREDIT"] = "Залік";
+localDisciplineUkr["DIFFERENTIATED_CREDIT"] = "Диф. залік";
+localDisciplineUkr["COURSE_PROJECT"] = "Курсовий проект";
+localDisciplineUkr["NON_GRADABLE"] = "Без оцінювання";
+//UkrToEng
