c3bfffeb1ac6f97674eae13a3e0b4f4e985c8e0c
Olena <alenagiv@gmail.com>
2016-12-16 03:25:49 +0200

    disciplines add

---
+++ src/main/java/com/coursed/controller/mvc/DisciplinesController.java
@@ -0,0 +1,23 @@
+package com.coursed.controller.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by alena on 15.12.2016.
+ */
+
+@Controller
+public class DisciplinesController {
+
+    @GetMapping("/disciplines")
+    public String getPage(Model model/*, @RequestParam(name = "planId") Long planId*/)
+    {
+        //model.addAttribute("planId", planId);
+        return "disciplines";
+    }
+}
