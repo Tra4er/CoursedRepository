ce9f39f27fff6680034a039afa00ba7a43b2bf33
Olena <alenagiv@gmail.com>
2016-12-19 00:52:03 +0200

    events add

---
+++ src/main/resources/static/js/local/events-local-ukr.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alena on 18.12.2016.
+ */
+var plannedEventType = {};
+//EngToUkr
+plannedEventType["ATTESTATION_FIRST"] = "Атестація №1";
+plannedEventType["ATTESTATION_SECOND"] = "Атестація №2";
+plannedEventType["GRADING_WEEK"] = "Залікова сесія";
+plannedEventType["EXAMINATION"] = "Екзаменаційна сесія";
+
--- src/main/resources/static/js/specific/events/events-teacher.js
+++ src/main/resources/static/js/specific/events/events-teacher.js
@@ -1,24 +1,41 @@
 /**
  * Created by Trach on 12/12/2016.
  */
-$(document).ready(init);
+// $(document).ready(init);
+//
+// function init(){
+//     fillGroupTable();
+//
+//     $("#plannedEvents").one("click", fillEventsTable);
+// }
+//
+// function fillGroupTable() {
+//     var titles = ['id', 'Номер', 'Спеціальність', 'Курс', 'Тип навчання', 'Ступінь'];
+//     insertTable(titles, "content-table-groups");
+//     var entityParams = ['id', 'number', 'speciality', 'courseNumber', 'groupType', 'groupDegree'];
+//     fillTableFromWithLinks("content-table-groups", API + "/groups/getAllWithoutCurators", entityParams, "number");
+// }
+//
+// function fillEventsTable() {
+//     var titles = ['id', 'Тип', 'Дата початку', 'Дата завершення'];
+//     insertTable(titles, "content-table-events");
+//     var entityParams = ['id', 'eventType', 'beginDate', 'expirationDate'];
+//     fillTableFromWithLinks("content-table-events", API + "/events/getAll", entityParams, "eventType");
+// }
 
-function init(){
-    fillGroupTable();
+// /api/discipline/getAllActualConnectedWithTeacher
+// plannedEventId
+// teacherId
 
-    $("#plannedEvents").one("click", fillEventsTable);
-}
 
-function fillGroupTable() {
-    var titles = ['id', 'Номер', 'Спеціальність', 'Курс', 'Тип навчання', 'Ступінь'];
-    insertTable(titles, "content-table-groups");
-    var entityParams = ['id', 'number', 'speciality', 'courseNumber', 'groupType', 'groupDegree'];
-    fillTableFromWithLinks("content-table-groups", API + "/groups/getAllWithoutCurators", entityParams, "number");
-}
 
-function fillEventsTable() {
-    var titles = ['id', 'Тип', 'Дата початку', 'Дата завершення'];
-    insertTable(titles, "content-table-events");
-    var entityParams = ['id', 'eventType', 'beginDate', 'expirationDate'];
-    fillTableFromWithLinks("content-table-events", API + "/events/getAll", entityParams, "eventType");
-}
+// $(function(){
+//     $.getJSON('/api/events/getAllFromCurrentYear', function(data){
+//
+//     });
+// // $.getJSON('/api/discipline/getAllActualConnectedWithTeacher', {teacherId: 1}, function(){
+// //
+// // });
+// });
+
+
