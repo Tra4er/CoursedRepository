3d5438783c28ed2140b42bd23054599c44702d24
Tra4er <brysonua@gmail.com>
2016-12-23 04:37:44 +0200

    Refactoring: registration validation. Added phone validation.

--- src/main/resources/static/js/specific/registration-validation.js
+++ src/main/resources/static/js/specific/registration-validation.js
@@ -2,49 +2,65 @@ $(document).ready(init);
 
 var emailRegex = /^[_A-Za-z0-9-]+(\.[_A-Za-z0-9-]+)*\@[A-Za-z]*\.[A-Za-z]{2,5}$/;
 var passRegex = /((?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,20})/;
+var phoneNumberRegex = /^(\+380)[0-9]{9}/;
 var person;
 
 function init(){
 
     // get person
     $("#registration-student").click(function () {
-        person = "Student";
+        person = "student";
     });
     $("#registration-teacher").click(function () {
-        person = "Teacher";
+        person = "teacher";
     });
 
     // Student check
-    $("#emailFieldStudent").focusout(checkEmail);
-    $("#passwordFieldStudent").focusout(checkPassword);
-    $("#confirmPasswordFieldStudent").focusout(checkConfirmPassword);
+    $("#phoneNumber-student").focusout(checkPhoneNumber);
+    $("#emailField-student").focusout(checkEmail);
+    $("#passwordField-student").focusout(checkPassword);
+    $("#confirmPasswordField-student").focusout(checkConfirmPassword);
 
     // Teacher check
-    $("#emailFieldTeacher").focusout(checkEmail);
-    $("#passwordFieldTeacher").focusout(checkPassword);
-    $("#confirmPasswordFieldTeacher").focusout(checkConfirmPassword);
+    $("#phoneNumber-teacher").focusout(checkPhoneNumber);
+    $("#emailField-teacher").focusout(checkEmail);
+    $("#passwordField-teacher").focusout(checkPassword);
+    $("#confirmPasswordField-teacher").focusout(checkConfirmPassword);
 }
 
 function checkFirstName() {
 }
 
+function checkPhoneNumber() {
+    var number = $("#phoneNumber-" + person).val();
+    var resultMessage = $("#result-phoneNumber-" + person);
+    if(number == "+380") {
+        resultMessage.text("Це поле обов'язкове.")
+    } else if(!phoneNumberRegex.test(number)) {
+        resultMessage.text("Номер телефону введено невірно.");
+    } else {
+        resultMessage.text("");
+    }
+}
+
 function checkEmail(){
-    var email = $("#emailField" + person).val();
+    var email = $("#emailField-" + person).val();
+    var resultMessage = $("#result-emailField-" + person);
     var availableEmail = false;
     if(email == "") {
-        $("#checkEmailResult" + person).text("Це поле не може бути пустим.");
+        resultMessage.text("Це поле не може бути пустим.");
         availableEmail = false;
     } else if(!emailRegex.test(email)) {
-        $("#checkEmailResult" + person).text("Емейл введено невірно.");
+        resultMessage.text("Емейл введено невірно.");
         availableEmail = false;
     } else { // Passed regex test
         $.ajax({ url: "/api/users/checkEmail", async: false, type: "get", data: "email=" + email})
             .done(function(response) {
                 if(response){
-                    $("#checkEmailResult" + person).text("Емейл існує");
+                    resultMessage.text("Емейл існує");
                     availableEmail = false;
                 } else {
-                    $("#checkEmailResult" + person).text("");
+                    resultMessage.text("");
                     availableEmail = true;
                 }
             });
@@ -53,40 +69,42 @@ function checkEmail(){
 }
 
 function checkPassword() {
-    var password = $("#passwordField" + person).val();
+    var password = $("#passwordField-" + person).val();
+    var resultMessage = $("#result-passwordField-" + person);
     if(password == "") {
-        $("#passwordResult" + person).text("Це поле не може бути пустим.");
+        resultMessage.text("Це поле не може бути пустим.");
         return false;
     } else if (!passRegex.test(password)) {
-        $("#passwordResult" + person).text("Занадто простий пароль.");
+        resultMessage.text("Занадто простий пароль.");
         return false;
     } else { // Passed regex test
-        $("#passwordResult" + person).text("");
+        resultMessage.text("");
         return true;
     }
 }
 
 function checkConfirmPassword() {
-    var password = $("#passwordField" + person).val();
-    var confirmPassword = $("#confirmPasswordField" + person).val();
+    var password = $("#passwordField-" + person).val();
+    var confirmPassword = $("#confirmPasswordField-" + person).val();
+    var resultMessage = $("#result-confirmPasswordField-" + person);
     if(password != confirmPassword) {
-        $("#confirmPasswordResult" + person).text("Паролі не співпадають.")
+        resultMessage.text("Паролі не співпадають.")
         return false;
     } else {
-        $("#confirmPasswordResult" + person).text("")
+        resultMessage.text("")
         return true;
     }
 }
 
 function Validate(){
     if(!checkEmail()){
-        $("#emailField" + person).focus();
+        $("#emailField-" + person).focus();
         return false;
     } else if(!checkPassword()) {
-        $("#passwordField" + person).focus();
+        $("#passwordField-" + person).focus();
         return false;
     } else if(!checkConfirmPassword()) {
-        $("#confirmPasswordField" + person).focus();
+        $("#confirmPasswordField-" + person).focus();
         return false;
     }
     return true;
