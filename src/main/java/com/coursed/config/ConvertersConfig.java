065df6cbb885d16cd33c78bb9f400a6f99f10569
Hexray <zikfrostfire@gmail.com>
2016-12-26 01:55:33 +0200

    Added hibernate dependency which can avoid json serialization of lazy-loaded fields

--- src/main/java/com/coursed/config/ConvertersConfig.java
+++ src/main/java/com/coursed/config/ConvertersConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.MapperFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,6 +32,9 @@ public class ConvertersConfig extends WebMvcConfigurerAdapter {
         //mapper.configure(MapperFeature.DEFAULT_VIEW_INCLUSION, false);
         mapper.registerModule(new JavaTimeModule());
         mapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+        //Allows JSON to avoid serialization of LAZY options
+        //mapper.registerModule(new Hibernate5Module());
+
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter(mapper);
         return converter;
     }
