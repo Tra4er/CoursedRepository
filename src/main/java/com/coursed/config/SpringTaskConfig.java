3cba8e351108ca47218e4e7f650ff2555632d956
Tra4er <brysonua@gmail.com>
2017-01-02 14:37:59 +0200

    Added tokens purge.

---
+++ src/main/java/com/coursed/config/SpringTaskConfig.java
@@ -0,0 +1,12 @@
+package com.coursed.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Created by Trach on 1/2/2017.
+ */
+@Configuration
+@EnableScheduling
+public class SpringTaskConfig {
+}
---
+++ src/main/java/com/coursed/task/TokensPurgeTask.java
@@ -0,0 +1,34 @@
+package com.coursed.task;
+
+import com.coursed.repository.PasswordResetTokenRepository;
+import com.coursed.repository.VerificationTokenRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.Date;
+
+/**
+ * Created by Trach on 1/2/2017.
+ */
+@Service
+@Transactional
+public class TokensPurgeTask {
+
+    @Autowired
+    private VerificationTokenRepository tokenRepository;
+
+    @Autowired
+    private PasswordResetTokenRepository passwordTokenRepository;
+
+    @Scheduled(cron = "${purge.cron.expression}")
+    public void purgeExpired() {
+
+        Date now = Date.from(Instant.now());
+
+        passwordTokenRepository.deleteAllExpiredSince(now);
+        tokenRepository.deleteAllExpiredSince(now);
+    }
+}
